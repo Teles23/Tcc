be6aa6e988c4573069bc29334dea5ff77350cc73
Teles23 <92751944+Teles23@users.noreply.github.com>
2022-12-18 11:38:32 -0300

    criptografia de senha

--- admin/cad_userOk.php
+++ admin/cad_userOk.php
@@ -2,45 +2,38 @@
 
 include "../config/connection.php";
 $dados = filter_input_array(INPUT_POST, FILTER_DEFAULT);
-// $nome = $_POST['nome'];
-// $email = $_POST['email'];
-// $senha = md5($_POST['senha']);
-// $confirmesenha = md5($_POST['confirme_senha']);
-// $perfil = ($_POST['perfil']);
+
 
 if (empty($dados['nome'])) {
     $retorna = ['status' => false, 'msg' => "Erro: Necessario preencher o campo nome!"];
 } else if (empty($dados['email'])) {
     $retorna = ['status' => false, 'msg' => "Erro: Necessario preencher o campo email!"];
-} else if (empty($dados['senha'] )) {
+} else if (empty($dados['senha'])) {
     $retorna = ['status' => false, 'msg' => "Erro: Necessario preencher o campo senha!"];
 } elseif (empty($dados['confirme_senha'])) {
     $retorna = ['status' => false, 'msg' => "Erro: Necessario preencher o campo Confirme senha!"];
 } else {
+    $senha = md5($_POST['senha']);
+    $confirmesenha = md5($_POST['confirme_senha']);
     //preparar
     $query_usuario = ("INSERT INTO users (nome, email, senha, confirmesenha, perfil) VALUES(:nome, :email, :senha, :confirmesenha, :perfil)");
     $stmt = $conectar->prepare($query_usuario);
     //tratar
     $stmt->bindParam(':nome', $dados['nome']);
     $stmt->bindParam(':email', $dados['email']);
-    $stmt->bindParam(':senha', $dados['senha']);
-    $stmt->bindParam(':confirmesenha', $dados['confirme_senha']);
+    $stmt->bindParam(':senha', $senha);
+    $stmt->bindParam(':confirmesenha', $confirmesenha);
     $stmt->bindParam(':perfil', $dados['perfil']);
 
     //executar
     $stmt->execute();
 
-    if($stmt->rowCount()){
+    if ($stmt->rowCount()) {
         $retorna = ['status' => true, 'msg' => "Usuario cadastro com sucesso!"];
-    }else{
+    } else {
         $retorna = ['status' => false, 'msg' => "Erro: Usuario não cadastro com sucesso!"];
     }
-
+    // header("Location:../pages/login.php");
 }
 echo json_encode($retorna);
 
-
-
-
-
-// header("Location:../pages/login.php");
